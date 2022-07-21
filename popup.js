ae42c94d8daae51fb180e540a5c266870bcb34e9
markjosephs <markjosephserrano123@gmail.com>
2022-07-21 12:54:52 +0800

    Added changes

--- popup.js
+++ popup.js
@@ -1,34 +1,37 @@
 const popup = document.querySelector('#nav-bar > li.get-started');
-const showStudent = document.querySelector('#student-form');
-const showAdmin = document.querySelector('#admin-form');
+const showStudent = document.querySelector('.form-wrapper #student-form');
+const showAdmin = document.querySelector('.form-wrapper #admin-form');
+const formWrapper = document.querySelector('.form-wrapper');
 
 popup.addEventListener('click', e => {
      e.preventDefault();
      if(e.target.classList.contains('get-started')) {
-         showStudent.style.display = 'block';
+        formWrapper.style.display = 'block';
+        showStudent.style.display = 'block';
     }
 });
 
 /* <-------- Close button ---------- > */
 
-const exitStudent = document.querySelector('#student-form .container-form .close');
-const exitAdmin = document.querySelector('#admin-form .container-form .close');
+const exitStudent = document.querySelector('.form-wrapper #student-form .container-form .close');
+const exitAdmin = document.querySelector('.form-wrapper #admin-form .container-form .close');
 
 exitStudent.addEventListener('click', e => {
     e.preventDefault();
     showStudent.style.display = 'none'; 
+    formWrapper.style.display = 'none';
 });
 
 exitAdmin.addEventListener('click', e => {
     e.preventDefault();
     showAdmin.style.display = 'none';
-    
+    formWrapper.style.display = 'none';
 });
 
 
 /* <------ Switch Login Form ------ > */
- const switchAdmin = document.querySelector('#student-form > div > form > div.switch-user > h1 > span');
- const switchStudent = document.querySelector('#admin-form > div > form > div.switch-user > h1 > span');
+ const switchAdmin = document.querySelector('.form-wrapper #student-form > div > form > div.switch-user > h1 > span');
+ const switchStudent = document.querySelector('.form-wrapper #admin-form > div > form > div.switch-user > h1 > span');
 
  switchAdmin.addEventListener('click', e => {
      e.preventDefault();
